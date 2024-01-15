be98fdd5e220670341bf62f4adc2acdbcf1cba9c
SunnyVirgo <sunny.virgo@antiquespride.edu.ph>
2024-01-15 16:10:34 +0800

    add tic_tac_toe structure and design

---
+++ script.js
@@ -0,0 +1,6 @@
+const restartBtn = document.querySelector('.restartBtn')
+const winningStatus = document.querySelector('.winningStatus')
+const playerTurn = document.querySelector('.playerTurn')
+const boxes = Array.from(document.getElementsByClassName('box'))
+
+console.log(boxes)
