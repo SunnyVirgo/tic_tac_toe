9d830b6ca206e798d630c6b633b2f3c8ab698b2f
SunnyVirgo <sunny.virgo@antiquespride.edu.ph>
2024-01-16 21:27:39 +0800

    Add a functionality that a game will pick who will be the player to play first

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ const restartBtn = document.querySelector('.restartBtn')
 const winningStatus = document.querySelector('.winningStatus')
 const playerTurn = document.querySelector('.playerTurn')
 const boxes = Array.from(document.getElementsByClassName('box'))
-const PLAYER_YOU = "X"
-const  PLAYER_COMPUTER= "O"
+const timerText = document.getElementsByClassName('timer')
+const PLAYER_YOU = 'X'
+const  PLAYER_COMPUTER= 'O'
+
+const players = [PLAYER_YOU,PLAYER_COMPUTER]
 
 let spaces = Array(9).fill(null)
-let currentPlayer = PLAYER_YOU
+let currentPlayer = players[Math.floor(Math.random()*players.length)]
 
 const winningPattern = [
     [0,1,2],
@@ -18,17 +21,40 @@ const winningPattern = [
     [0,4,8],
     [2,4,6]    
 ]
+function startGame(player){
 
-function startGame(){
-    
-    for (let index = 0; index < boxes.length; index++) {
-        const box = document.getElementById(boxes[index].id);
-
-        box.addEventListener('click', function(e){
-            boxClicked(e)
-        })
+    if (player === PLAYER_YOU) {
+       humanPlayer(player)
+    } else {
+        for (let index = 0; index < boxes.length; index++) {
+            const element = boxes[index];
+            element.style.pointerEvents='none'
+        }
+        setTimeout(() =>{
+            computerTurn(player)
+        },2000)
     }
+    
+    
     playerTurn.innerText = currentPlayer === PLAYER_YOU ? 'Your Turn' : `Computer's Turn`
+}
+
+function humanPlayer(player){
+    if(player === PLAYER_YOU){
+        for (let index = 0; index < boxes.length; index++) {
+            const box = document.getElementById(boxes[index].id);
+    
+            box.addEventListener('click', function(e){
+                boxClicked(e)
+            })
+        }
+    }else{
+        for (let index = 0; index < boxes.length; index++) {
+            const element = boxes[index];
+            element.style.pointerEvents='none'
+        }
+    }
+    
 }
 
 
@@ -72,56 +98,59 @@ function boxClicked(e){
     
     
             setTimeout(() =>{
-                computerTurn()
+                computerTurn(currentPlayer)
             },2000)
         }  
     }
   
    
 }
-function computerTurn(){
-    if(!playerWins()){
-        var emptyBoxes = boxes.filter(box=> box.innerText==="")
-        if(emptyBoxes.length > 0){
-          randomBox = emptyBoxes[Math.floor(Math.random()*emptyBoxes.length)]
-         const id = randomBox.id
-         if(spaces[id] === null){
-             spaces[id] = currentPlayer
-     
-             randomBox.innerText = currentPlayer
-     
-             if(playerWins()){
-                 winningStatus.innerText =  currentPlayer === PLAYER_YOU ? 'You Won!' : `You Loose!`
-                 let winningBlocks = playerWins()
-                 winningBlocks.map(boxId=>boxes[boxId].style.backgroundColor = 'white')
-     
-                 setTimeout(() => {
-                     restartGame()
-                 }, 5000);
-                 
-                }
-                if(!spaces.includes(null) && playerWins() == false){
-                 winningStatus.innerText = `Draw!`
-                 setTimeout(() => {
-                     restartGame()
-                 }, 5000);
-                }
-     
-             currentPlayer = currentPlayer == PLAYER_COMPUTER ? PLAYER_YOU : PLAYER_COMPUTER
-     
-             playerTurn.innerText = currentPlayer === PLAYER_YOU ? 'Your Turn' : `Computer's Turn`
-     
-             for (let index = 0; index < boxes.length; index++) {
-                 const element = boxes[index];
-                 element.style.pointerEvents='auto'
+function computerTurn(player){
+    if(player === PLAYER_COMPUTER){
+        if(!playerWins()){
+            var emptyBoxes = boxes.filter(box=> box.innerText==="")
+            if(emptyBoxes.length > 0){
+              randomBox = emptyBoxes[Math.floor(Math.random()*emptyBoxes.length)]
+             const id = randomBox.id
+             if(spaces[id] === null){
+                 spaces[id] = currentPlayer
+         
+                 randomBox.innerText = currentPlayer
+         
+                        if(playerWins()){
+                            winningStatus.innerText =  currentPlayer === PLAYER_YOU ? 'You Won!' : `You Loose!`
+                            let winningBlocks = playerWins()
+                            winningBlocks.map(boxId=>boxes[boxId].style.backgroundColor = 'rgb(126, 123, 103')
+                
+                            setTimeout(() => {
+                                restartGame()
+                            }, 5000);
+                            
+                            }
+                            if(!spaces.includes(null) && playerWins() == false){
+                            winningStatus.innerText = `Draw!`
+                            setTimeout(() => {
+                                restartGame()
+                            }, 5000);
+                            }
+         
+                 currentPlayer = currentPlayer == PLAYER_COMPUTER ? PLAYER_YOU : PLAYER_COMPUTER
+         
+                 playerTurn.innerText = currentPlayer === PLAYER_YOU ? 'Your Turn' : `Computer's Turn`
+
+                 humanPlayer(currentPlayer)
+         
+                 for (let index = 0; index < boxes.length; index++) {
+                     const element = boxes[index];
+                     element.style.pointerEvents='auto'
+                 }
+         
              }
-     
-         }
+            }
         }
     }
-   
-
 }
+
 
 function restartGame(){
     for (let index = 0; index < boxes.length; index++) {
@@ -130,13 +159,12 @@ function restartGame(){
         let box = document.getElementById(element)
         box.innerText=""
         spaces.fill(null)
-        currentPlayer = PLAYER_YOU
         box.style.backgroundColor='transparent'
         boxxx.style.pointerEvents='auto'
-        winningStatus.innerText = ""
-        
+        winningStatus.innerText = ""  
     }
-    startGame()
+    currentPlayer = players[Math.floor(Math.random()*players.length)]
+    startGame(currentPlayer)
 }
 restartBtn.addEventListener('click', function() {
     restartGame()
@@ -154,4 +182,4 @@ function playerWins(){
     }
     return false
 }
-startGame()
+startGame(currentPlayer)
